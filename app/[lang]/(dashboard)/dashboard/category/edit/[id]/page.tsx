32be46167f9a233635960a405eeab77e3ca4eb3d
TahirMuradov <muradovtahir01@gmail.com>
2024-09-08 22:00:26 +0400

    "Refactored CategoryEditForm page: removed unnecessary whitespace, reformatted code, and updated function signature"

--- app/[lang]/(dashboard)/dashboard/category/edit/[id]/page.tsx
+++ app/[lang]/(dashboard)/dashboard/category/edit/[id]/page.tsx
@@ -1,13 +1,8 @@
+import CategoryEditForm from "@/dashboardComponents/CategoryComponents/CategoryEditForm";
+import { Locale } from "@/i18n-config";
 
-import CategoryEditForm from '@/dashboardComponents/CategoryComponents/CategoryEditForm';
-import { Locale } from '@/i18n-config'
-
-const page = (params:{lang:Locale,id:string}) => {
-  const apiDomen = process.env.apiDomen;
-
-  return (
-<CategoryEditForm params={{id:params.id,lang:params.lang,apiDomen}}/>
-  )
+const page:React.FC<{params:{lang:Locale,id:string}}>=({params:{id,lang}})=>{
+    const apiDomen = process.env.apiDomen;
+ return   <CategoryEditForm params={{id:id,lang:lang,apiDomen}}/>
 }
-
 export default page
